5f9cb0925ea3715073e4772956a149526c3d3651
olinaresDall <octavio9713@gmail.com>
2020-11-25 11:35:33 -0300

    Added sonar effect and sample scenes for testing purposes

---
+++ Assets/Scripts/S_soundEmitter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_soundEmitter : SimpleSonarShader_Object
+{
+    [SerializeField]
+    private float _delayTimer = 1f;
+    private float _elapseTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapseTime += Time.deltaTime;
+        if (_elapseTime >= _delayTimer)
+        {
+            _elapseTime = _elapseTime % 1f;
+            this.StartSonarRing(this.transform.position, 100 / 10.0f);
+        }
+    }
+}
