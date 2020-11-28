f27277c39eb0735290b9b2bd0e3189880d8e34f1
IanFMartin <32570728+IanFMartin@users.noreply.github.com>
2020-11-28 04:05:49 -0300

    Revert "sound of defeat"
    This reverts commit 96331c50f0f0cbd0c07b8d4d3aff50925c65233d.

--- Assets/Scripts/GameOverlapss.cs
+++ Assets/Scripts/GameOverlapss.cs
@@ -7,22 +7,17 @@ using UnityEngine.SceneManagement;
 public class GameOverlapss : MonoBehaviour
 {
     public Image gameOverImage;
-    AudioSource _as;
     // Start is called before the first frame update
     void Start()
     {
 
         gameOverImage.gameObject.SetActive(false);
-        _as = GetComponent<AudioSource>();
         StartCoroutine(GameOver());
     }
 
    IEnumerator GameOver()
     {
-        yield return new WaitForSeconds(0.75f);
-        _as.Play();
-
-        yield return new WaitForSeconds(1.75f);
+        yield return new WaitForSeconds(2.5f);
         gameOverImage.gameObject.SetActive(true);
         Cursor.visible = true;
     }
