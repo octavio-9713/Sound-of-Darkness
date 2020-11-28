4a6e131858209b121b01b969fb44d347a74fc59c
IanFMartin <32570728+IanFMartin@users.noreply.github.com>
2020-11-28 04:24:36 -0300

    meti puerta, cambie le auto, ajuste un poco  la escena de perder

--- Assets/Scripts/GameOverlapss.cs
+++ Assets/Scripts/GameOverlapss.cs
@@ -6,16 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class GameOverlapss : MonoBehaviour
 {
+    AudioSource _as;
     public Image gameOverImage;
+    public Transform cam;
+    public Transform whereTo;
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
 
+        _as = GetComponent<AudioSource>();
         gameOverImage.gameObject.SetActive(false);
         StartCoroutine(GameOver());
     }
+    private void Update()
+    {
 
-   IEnumerator GameOver()
+        cam.transform.position = Vector3.Lerp(cam.transform.position, whereTo.transform.position, speed * Time.deltaTime);
+    }
+    IEnumerator GameOver()
     {
         yield return new WaitForSeconds(2.5f);
         gameOverImage.gameObject.SetActive(true);
